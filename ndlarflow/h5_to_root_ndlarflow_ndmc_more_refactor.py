fefdeb1497880b254b452788c842f4540e680b00
Bruce Howard <howard@fnal.gov>
2025-02-21 11:32:33 -0500

    Add option to use for 2x2 and the corresponding difference in the true neutrino vertex position saving. Add in a usage/help statement based on input arguments.

--- ndlarflow/h5_to_root_ndlarflow_ndmc_more_refactor.py
+++ ndlarflow/h5_to_root_ndlarflow_ndmc_more_refactor.py
@@ -15,10 +15,21 @@ import sys
 # see https://stackoverflow.com/questions/72187937/writing-trees-number-of-baskets-and-compression-uproot
 
 # Main function with command line settable params
+def printUsage():
+    print('python h5_to_root_ndlarflow_ndmc_more_refactor.py FileList IsData IsFinalHits Is2x2 OutName')
+    print('-- Parameters')
+    print('FileList    [REQUIRED]:                           comma separated set of files to convert - note it will be one output')
+    print('IsData      [OPTIONAL, DEFAULT = 0, is MC]:       1 = Data, otherwise = MC')
+    print('IsFinalHits [OPTIONAL, DEFAULT = 0, prompt hits]: 1 = use "final" hits, otherwise = "prompt"')
+    print('Is2x2       [OPTIONAL, DEFAULT = 0, is NDLAr]:    1 = use 2x2 file customizations, 0 = use NDLAr file customizations')
+    print('OutName     [OPTIONAL, DEFAULT = input[0]+stuff]: string for an output file name if you want to override.')
+    print('')
+
 def main(argv=None):
     fileNames=[]
     useData=False
     useFinalHits=False
+    use2x2=False
     overrideOutname=1
     outname=''
 
@@ -27,10 +38,20 @@ def main(argv=None):
     trueYOffset=0#42+268
     trueZOffset=0#-1300
 
-    if len(sys.argv==1):
-        print('Must at least pass a file location/name to be converted')
+    if len(sys.argv)==1:
+        print('---------------------------------------------------------------')
+        print('Must at least pass a file location/name to be converted, usage:')
+        print('---------------------------------------------------------------')
+        printUsage()
+        return
     if len(sys.argv)>1:
-        if sys.argv[1]!=None:
+        if str(sys.argv[1])=='help' or str(sys.argv[1])=='h' or str(sys.argv[1])=='-h' or str(sys.argv[1])=='--help':
+            print('---------------------------------------------------------------')
+            print('usage:')
+            print('---------------------------------------------------------------')
+            printUsage()
+            return
+        elif sys.argv[1]!=None:
             fileList=str(sys.argv[1])
             fileNames=fileList.split(',')
         if len(sys.argv)>2 and sys.argv[2]!=None:
@@ -40,6 +61,9 @@ def main(argv=None):
             if int(sys.argv[3])==1:
                 useFinalHits=True
         if len(sys.argv)>4 and sys.argv[4]!=None:
+            if int(sys.argv[4])==1:
+                use2x2=True
+        if len(sys.argv)>5 and sys.argv[5]!=None:
             outname=str(sys.argv[4])
             overrideOutname=0
 
@@ -145,9 +169,14 @@ def main(argv=None):
                 vertex_indicesArray = np.where(flow_out["/mc_truth/interactions/data"]["event_id"] == spillID)[0]
                 vtx = flow_out["/mc_truth/interactions/data"][vertex_indicesArray]
                 nu_vtx_id = (vtx['vertex_id']).astype('int64')
-                nu_vtx_x = (vtx['x_vert']).astype('float32')
-                nu_vtx_y = (vtx['y_vert']).astype('float32')
-                nu_vtx_z = (vtx['z_vert']).astype('float32')
+                if use2x2==True:
+                    nu_vtx_x = (vtx['vertex'][:,0]).astype('float32')
+                    nu_vtx_y = (vtx['vertex'][:,1]).astype('float32')
+                    nu_vtx_z = (vtx['vertex'][:,2]).astype('float32')
+                else:
+                    nu_vtx_x = (vtx['x_vert']).astype('float32')
+                    nu_vtx_y = (vtx['y_vert']).astype('float32')
+                    nu_vtx_z = (vtx['z_vert']).astype('float32')
                 nu_vtx_E = (vtx['Enu']*MeV2GeV).astype('float32')
                 nu_spill_t = (vtx['t_event']).astype('float64')
                 nu_pdg = (vtx['nu_pdg']).astype('int32')
