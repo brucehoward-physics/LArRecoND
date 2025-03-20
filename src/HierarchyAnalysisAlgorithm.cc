1e649f3612b098d0f1f4e1f76511616cec105e8d
Bruce Howard <61521590+brucehoward-physics@users.noreply.github.com>
2025-03-20 11:26:45 -0400

    Forgot to bring over the part where recoPfoId is declared.

--- src/HierarchyAnalysisAlgorithm.cc
+++ src/HierarchyAnalysisAlgorithm.cc
@@ -254,6 +254,8 @@ void HierarchyAnalysisAlgorithm::EventAnalysisOutput(const LArHierarchyHelper::M
         LArHierarchyHelper::RecoHierarchy::NodeVector recoNodes;
         recoHierarchy.GetFlattenedNodes(pRoot, recoNodes);
 
+        int recoPfoId = -1;
+
         // Loop over the reco nodes
         for (const LArHierarchyHelper::RecoHierarchy::Node *pRecoNode : recoNodes)
         {
